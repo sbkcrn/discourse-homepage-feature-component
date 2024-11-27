93eaa9b827379d0a7a50eb8c0aa45f77639b9aea
sbkcrn <simonbalmain@rabbit.tech>
2024-11-27 22:46:48 +0000

    Update featured-homepage-topics.js

--- javascripts/discourse/components/featured-homepage-topics.js
+++ javascripts/discourse/components/featured-homepage-topics.js
@@ -10,31 +10,35 @@ export default Component.extend({
 
     // Fetch topics if featured_tag is set
     if (settings.featured_tag) {
+      const tags = settings.featured_tag.split(",").map((tag) => tag.trim());
       const sortOrder = settings.sort_by_created ? "created" : "activity";
-      const topicList = await this.store.findFiltered("topicList", {
-        filter: "latest",
-        params: {
-          tags: [`${settings.featured_tag}`],
-          order: sortOrder,
-        },
-      });
 
-      bannerItems.push(
-        ...topicList.topics
-          .filter(
-            (topic) =>
-              topic.image_url &&
-              (!settings.hide_closed_topics || !topic.closed)
-          )
-          .slice(0, settings.number_of_topics)
-          .map((topic) => ({
-            type: "topic",
-            id: topic.id,
-            title: topic.title,
-            image_url: topic.image_url,
-            url: `/t/${topic.slug}/${topic.id}`,
-          }))
-      );
+      for (const tag of tags) {
+        const topicList = await this.store.findFiltered("topicList", {
+          filter: "latest",
+          params: {
+            tags: [tag],
+            order: sortOrder,
+          },
+        });
+
+        bannerItems.push(
+          ...topicList.topics
+            .filter(
+              (topic) =>
+                topic.image_url &&
+                (!settings.hide_closed_topics || !topic.closed)
+            )
+            .slice(0, settings.number_of_topics)
+            .map((topic) => ({
+              type: "topic",
+              id: topic.id,
+              title: topic.title,
+              image_url: topic.image_url,
+              url: `/t/${topic.slug}/${topic.id}`,
+            }))
+        );
+      }
     }
 
     // Fetch categories if configured
